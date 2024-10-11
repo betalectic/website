e32fab70bd264c8bcc866a53a4d35785a315c31f
Abhishek <159003057+Abhishek-Beta@users.noreply.github.com>
2024-10-11 17:56:33 +0530

    Added pattern (#11)

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,18 +1,63 @@
+import Link from "@docusaurus/Link";
 import Layout from "@theme/Layout";
 
 export default function Contact() {
   return (
     <Layout title="Contact" description="Contact Betalectic">
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "50vh",
-          fontSize: "20px",
-        }}
-      >
-        <p>Contact Betalectic</p>
+      <div className="container py-28">
+        <div className="max-w-3xl mx-auto">
+          <div className="flex flex-col justify-center">
+            <h1>Get in Touch with Us</h1>
+            <p>
+              How can we help you? Leave your contact details and we will get
+              back to you..
+            </p>
+            <div className="flex gap-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-12"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21"
+                />
+              </svg>
+
+              <p className="font-bold my-auto">
+                3rd Floor, Mitti’s Building, Near Divyashree Lanco Hills,
+                Chaitanya Enclave, Manikonda, Hyderabad, Telangana - 500089
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-10"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M16.5 12a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 1 0-2.636 6.364M16.5 12V8.25"
+              />
+            </svg>
+
+            <Link
+              className="font-bold my-auto"
+              href="mailto:contact@betalectic.com"
+            >
+              contact@betalectic.com
+            </Link>
+          </div>
+        </div>
       </div>
     </Layout>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,70 +2,73 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import { GridPattern } from "../components/GridPattern";
+import { motion, MotionConfig, useReducedMotion } from "framer-motion";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
 
   return (
-    <header className="container py-10">
-      <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#3b82f6] to-[#9333ea] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-          ></div>
-        </div>
+    <header className="container max-w-7xl py-10">
+      <div className="relative isolate px-6 lg:px-8">
+        <div className="absolute inset-x-0 top-0 -z-100 h-[884px] overflow-hidden rounded-t-4xl bg-gradient-to-b from-neutral-50">
+          <MotionConfig reducedMotion="user">
+            <motion.div
+              layout
+              style={{ borderTopLeftRadius: 40, borderTopRightRadius: 40 }}
+              className="relative flex flex-auto overflow-hidden bg-white pt-14"
+            >
+              <motion.div
+                layout
+                className="relative isolate flex w-full flex-col pt-9"
+              >
+                <GridPattern
+                  className="absolute inset-x-0 -top-14 -z-10 h-[1000px] w-full fill-neutral-50 stroke-neutral-950/5 [mask-image:linear-gradient(to_bottom_left,white_40%,transparent_50%)]"
+                  yOffset={-96}
+                  interactive
+                />
 
-        <div className="max-w-xl py-16 sm:py-36">
-          <div className="">
-            <h1 className="text-5xl md:text-6xl lg:text-8xl py-3">
-              {siteConfig.title}
-            </h1>
-            <p className="text-2xl md:text-3xl lg:text-4xl font-semibold tracking-tight">
-              Transforming Ideas into Digital Solutions
-            </p>
-            <p>
-              Where innovation meets execution at full speed. we don’t just
-              build software we engineer impact. Our team thrives on turning
-              complex challenges into seamless, tailored solutions that move
-              fast, scale effortlessly.
-            </p>
+                <div className="max-w-7xl px-6 py-16 sm:py-36">
+                  <div className="">
+                    <h1 className="text-5xl md:text-6xl lg:text-8xl py-3">
+                      {siteConfig.title}
+                    </h1>
+                    <p className="text-2xl md:text-3xl lg:text-4xl font-semibold tracking-tight">
+                      Transforming Ideas into Digital Solutions
+                    </p>
+                    <p className="max-w-xl">
+                      Where innovation meets execution at full speed. we don’t
+                      just build software we engineer impact. Our team thrives
+                      on turning complex challenges into seamless, tailored
+                      solutions that move fast, scale effortlessly.
+                    </p>
 
-            <div className="mt-10 flex items-center gap-x-6">
-              <Link
-                to="/contact"
-                className="relative inline-block px-6 py-3 text-[#212121] font-extrabold text-[17px] 
+                    <div className="mt-10 flex items-center gap-x-6">
+                      <Link
+                        to="/contact"
+                        className="relative inline-block px-6 py-3 text-[#212121] font-extrabold text-[17px] 
         bg-[#e8e8e8] rounded-[15px] shadow-lg overflow-hidden 
         transition-all duration-300 ease-in-out group no-underline"
-              >
-                <span
-                  className="absolute inset-0 w-0 h-full bg-[#212121] rounded-[15px] 
+                      >
+                        <span
+                          className="absolute inset-0 w-0 h-full bg-[#212121] rounded-[15px] 
         transition-all duration-300 ease-in-out group-hover:w-full"
-                ></span>
-                <span className="relative z-10 transition-colors duration-300 ease-in-out group-hover:text-[#e8e8e8] no-underline">
-                  Contact us
-                </span>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#3b82f6] to-[#9333ea] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-          ></div>
+                        ></span>
+                        <span className="relative z-10 transition-colors duration-300 ease-in-out group-hover:text-[#e8e8e8] no-underline">
+                          Contact us
+                        </span>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </motion.div>
+            </motion.div>
+          </MotionConfig>
+
+          {/* <GridPattern
+            className="absolute inset-0 h-full w-full fill-neutral-100 stroke-neutral-950/5 [mask-image:linear-gradient(to_bottom_left,white_40%,transparent_50%)]"
+            yOffset={-270}
+          /> */}
         </div>
       </div>
     </header>
