ad47a2841b76a7b087e6439cc4d50323731077c6
Abhishek <abhishek@betalectic.com>
2024-10-11 10:22:01 +0530

    Implemented pr preview

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,6 +2,11 @@ import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
+const BASE_URL =
+  process.env.PR_NUMBER !== undefined
+    ? `/website/pr-preview/pr-${process.env.PR_NUMBER}/`
+    : "/website/";
+
 const config: Config = {
   title: "Betalectic",
   tagline: "Dinosaurs are cool",
@@ -11,7 +16,7 @@ const config: Config = {
   url: "https://github.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: process.env.BASE_URL || "/website/",
+  baseUrl: BASE_URL,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
