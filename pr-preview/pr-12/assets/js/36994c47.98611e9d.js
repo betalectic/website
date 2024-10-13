1a008ad6279a9651d15323248a7f80d22b9df00f
Abhishek-Beta <Abhishek-Beta@users.noreply.github.com>
2024-10-13 14:13:06 +0000

    Deploy preview for PR 12 🛫

---
+++ pr-preview/pr-12/assets/js/36994c47.98611e9d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbetalectic_website=self.webpackChunkbetalectic_website||[]).push([[858],{5516:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
